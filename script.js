50eee9c49d02ae104a61fdcea3b71706c768d7c4
RParkerYules <109665209+RParkerYules@users.noreply.github.com>
2022-08-17 07:02:13 +1000

    changes to resolve bug

--- script.js
+++ script.js
@@ -24,38 +24,25 @@ function writePassword() {
 function generatePassword() {
   // This next line of code is used to tell the user to add a value, and if they don't add a correct value. They will be reminded.
   while (true) {
-    var firstPrompt = prompt("How long would you like your password to be? Choose between 8 to 20 characters.");
-    if (!firstPrompt) {
+    var lengthPrompt = prompt("Password Length: Enter between 8 to 20 to define characters.");
+    if (!lengthPrompt) {
       alert("Please add a value");
-    } else if (firstPrompt < 8 || firstPrompt > 20) {
+    } else if (lengthPrompt < 8 || lengthPrompt > 20) {
       prompt("Please add a value higher than 8 or lower than 20");
-    } else {
-      break;
-    }
+    
   }
+  // There is a problem here, as the app is asking me to enter the numbet of chars, I enter them and it asks again.
+  // Then when I insert the second input it accepts, but doesn't run the code below to generate the password
+  // I need to fix that bug, and then add the logic to generate a random password
 
-  // This next line of code is used to ask the user how they would like their password to be like.
-  if (confirm("Do you want numbers?")) {
-    choices = choices.concat(number);
-  }
-  if (confirm("Do you want special characters?")) {
-    choices = choices.concat(spChar);
-  }
-  if (confirm("Do you want upper case characters?")) {
-    choices = choices.concat(upperCase);
-  }
-  if (confirm("Do you want lower case characters?")) {
-    choices = choices.concat(lowerCase);
-  }
-  // This next line of code is used in case no option has been chosen for the password generator
-  if (choices.length == 0) {
-    alert("Please choose a criteria");
-  }
+
+
+
 
   // empty array that will contain the new empty password
   var randomPassword = [];
   // This next line of code is the loop requiered for the generation of the password
-  for (var i = 0; i < firstPrompt; i++) {
+  for (var i = 0; i < lengthPrompt; i++) {
     var allChoices = choices[Math.floor(Math.random() * choices.length)];
     randomPassword.push(allChoices);
   }
