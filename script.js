872696df1fc57b9095d2efa89bcf6ffbc28b126f
RParkerYules <109665209+RParkerYules@users.noreply.github.com>
2022-08-17 23:46:08 +1000

    working but with a bug

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", ];
 // Array containing the special characters for the password
 var spChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-// This variable is used for the concanation of the variables later
+
+// This variable is used to concanate the numbers, upper case, lower case & special characters into one string
 
 var choices = (upperCase.concat(number).concat(spChar).concat(lowerCase));
 var totalChoices = choices.length
 
 
-// Write password to the #password input
+// Code to write the randomly generated password to the screen; 
+// Initiated via 'click' in HTML;
 function writePassword() {
-
   var password = generatePassword();
   console.log(password);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -33,30 +33,32 @@ function generatePassword() {
   while (true) {
     var lengthPrompt = prompt("Pleae enter a number between 8 to 20 for password length.");
 
+// If no input, then prompt to entewr a value
     if (!lengthPrompt) {
-      alert("Please add a value");
-    }
+      prompt("Please add a value");
+    }    
+
+// If a number less than 8, greater than 20, or a special char is entered, then this applies
     if (lengthPrompt < 8 || lengthPrompt > 20) {
-        prompt("Please add a value higher than 8 or lower than 20");
-    }
-    if (lengthPrompt >= 8 && lengthPrompt <= 20) {
-      
-      break;
-    }
-    
-   
+      prompt("Please add a value higher than 8 or lower than 20");
   }
 
+// If a valid number is entered, then this applies
+  if (lengthPrompt >= 8 && lengthPrompt <= 20) {
+      break;
+    } 
+  }
 
-  // empty array that will contain the new empty password
+// Function that will generate the new password - length is the number of chars entered
   var randomPassword = [];
-  // This next line of code is the loop requiered for the generation of the password
+
+// This next line of code is the loop requiered for the generation of the password
   for (var i = 0; i < lengthPrompt; i++) {
     var randomChoice = choices[Math.floor(Math.random() * totalChoices)];
     
     randomPassword.push(randomChoice);
   }
-
+// Display the randomly generated password
   return randomPassword.join("");
 }
 
